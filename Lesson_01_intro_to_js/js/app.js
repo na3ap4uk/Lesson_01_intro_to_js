f96e65d5faa695d860f45c382bc35ec4917ad77b
na3ap4uk <na3ap4uk@ukr.net>
2015-12-17 02:43:14 +0200

    Adding JS homework

---
+++ Lesson_01_intro_to_js/js/app.js
@@ -0,0 +1,42 @@
+
+var personname = 'Nazar';
+var time = '06:00';
+var place = 'Lviv';
+var event = prompt('When Nazar will arrive?');
+var sentense = personname + ' arrives to ' + place + ' at ' + time;
+console.log(sentense);
+
+var x1 = 5;
+var y1 = 15;
+var x2 = prompt('Enter x2:');
+var y2 = prompt('Enter y2:');
+var result = Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+alert(result);
+
+var output = '';
+for (var i = 0; i<9; i++){
+output = output + '#';
+console.log(output);
+}
+
+ var i = 0;
+ while(i<8){
+    if(i%2==0){
+		console.log("# # # #");
+	} else {
+		console.log(" # # # #");
+	}
+	i++;
+}
+
+var i = Math.ceil(Math.random()*100).toString();
+var a = prompt('Try to gues the number of monkeys!');
+while (i!==a){    
+    if(i>a){
+		a = prompt('Our number is higher, try again');   
+    } else if(i<a){
+		a = prompt('Our number is lower, try again');
+	}
+}
+console.log('You are right, the number is ' + a);
+
