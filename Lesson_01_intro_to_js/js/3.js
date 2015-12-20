1d0978cbe14cad548f9fc6d782fa67127274312a
na3ap4uk <na3ap4uk@ukr.net>
2015-12-20 05:54:49 +0200

    changes

---
+++ Lesson_01_intro_to_js/js/3.js
@@ -0,0 +1,9 @@
+'use strict':
+
+(function(){
+var output = '';
+for (var i = 0; i<9; i++){
+output = output + '#';
+console.log(output);
+}
+})();
--- Lesson_01_intro_to_js/js/app.js
+++ Lesson_01_intro_to_js/js/app.js
@@ -1,24 +1,32 @@
+/*
 
+(function(){
 var personname = 'Nazar';
 var time = '06:00';
 var place = 'Lviv';
 var event = prompt('When Nazar will arrive?');
 var sentense = personname + ' arrives to ' + place + ' at ' + time;
 console.log(sentense);
+})();
 
+(function(){
 var x1 = 5;
 var y1 = 15;
 var x2 = prompt('Enter x2:');
 var y2 = prompt('Enter y2:');
 var result = Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
 alert(result);
+})();
 
+(function(){
 var output = '';
 for (var i = 0; i<9; i++){
 output = output + '#';
 console.log(output);
 }
+})();
 
+(function(){
  var i = 0;
  while(i<8){
     if(i%2==0){
@@ -28,7 +36,9 @@ console.log(output);
 	}
 	i++;
 }
+})();
 
+(function(){
 var i = Math.ceil(Math.random()*100).toString();
 var a = prompt('Try to gues the number of monkeys!');
 while (i!==a){    
@@ -39,4 +49,6 @@ while (i!==a){
 	}
 }
 console.log('You are right, the number is ' + a);
+})();
 
+*/
