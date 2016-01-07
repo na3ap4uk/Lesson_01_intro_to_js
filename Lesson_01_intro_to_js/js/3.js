16ed220ab41e4f2fa8ea156d09d594edd5ee377d
na3ap4uk <na3ap4uk@ukr.net>
2016-01-07 12:34:31 +0200

    code formating

--- Lesson_01_intro_to_js/js/3.js
+++ Lesson_01_intro_to_js/js/3.js
@@ -1,9 +1,10 @@
 'use strict';
-
-(function(){
-var output = '';
-for (var i = 0; i<9; i++){
-output = output + '#';
-console.log(output);
-}
+(function() {
+	//---------------------------
+    var output = '';
+    for (var i = 0; i < 9; i++) {
+        output = output + '#';
+        console.log(output);
+    }
+    //---------------------------
 })();
--- Lesson_01_intro_to_js/js/app.js
+++ Lesson_01_intro_to_js/js/app.js
@@ -1,54 +1,58 @@
 /*
-
-(function(){
-var personname = 'Nazar';
-var time = '06:00';
-var place = 'Lviv';
-var event = prompt('When Nazar will arrive?');
-var sentense = personname + ' arrives to ' + place + ' at ' + time;
-console.log(sentense);
+(function() {
+	//---------------------------
+    var personname = 'Nazar';
+    var time = '06:00';
+    var place = 'Lviv';
+    var event = prompt('When Nazar will arrive?');
+    var sentense = personname + ' arrives to ' + place + ' at ' + time;
+    console.log(sentense);
+    //---------------------------
 })();
-
-(function(){
-var x1 = 5;
-var y1 = 15;
-var x2 = prompt('Enter x2:');
-var y2 = prompt('Enter y2:');
-var result = Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
-alert(result);
+(function() {
+	//---------------------------
+    var x1 = 5;
+    var y1 = 15;
+    var x2 = prompt('Enter x2:');
+    var y2 = prompt('Enter y2:');
+    var result = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    alert(result);
+    //---------------------------
 })();
-
-(function(){
-var output = '';
-for (var i = 0; i<9; i++){
-output = output + '#';
-console.log(output);
-}
+(function() {
+	//---------------------------
+    var output = '';
+    for (var i = 0; i < 9; i++) {
+        output = output + '#';
+        console.log(output);
+    }
+    //---------------------------
 })();
-
-(function(){
- var i = 0;
- while(i<8){
-    if(i%2==0){
-		console.log("# # # #");
-	} else {
-		console.log(" # # # #");
-	}
-	i++;
-}
+(function() {
+	//---------------------------
+    var i = 0;
+    while (i < 8) {
+        if (i % 2 === 0) {
+            console.log("# # # #");
+        } else {
+            console.log(" # # # #");
+        }
+        i++;
+    }
+    //---------------------------
 })();
-
-(function(){
-var i = Math.ceil(Math.random()*100).toString();
-var a = prompt('Try to gues the number of monkeys!');
-while (i!==a){    
-    if(i>a){
-		a = prompt('Our number is higher, try again');   
-    } else if(i<a){
-		a = prompt('Our number is lower, try again');
-	}
-}
-console.log('You are right, the number is ' + a);
+(function() {
+	//---------------------------
+    var i = Math.ceil(Math.random() * 100).toString();
+    var a = prompt('Try to gues the number of monkeys!');
+    while (i !== a) {
+        if (i > a) {
+            a = prompt('Our number is higher, try again');
+        } else if (i < a) {
+            a = prompt('Our number is lower, try again');
+        }
+    }
+    console.log('You are right, the number is ' + a);
+    //---------------------------
 })();
-
 */
